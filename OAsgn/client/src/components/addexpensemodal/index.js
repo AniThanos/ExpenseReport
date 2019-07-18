46c477d8b0032737bee8fca3dbba5c7dea77e1d5
Animesh <animesh.ranchi91@gmail.com>
2019-07-19 01:18:12 +0530

    Adding Expense Done

--- OAsgn/client/src/components/addexpensemodal/index.js
+++ OAsgn/client/src/components/addexpensemodal/index.js
@@ -1,34 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import axios from 'axios'
 
-export default class ExpenseForm extends React.Component {
-    render() {
-        return (
-            <div>
-                <Form>
-                    <Form.Group controlId="">
-                        <Form.Label>Category</Form.Label>
-                        <Form.Control type="text" placeholder="Enter category" />
-                    </Form.Group>
+const ExpenseForm = () => {
+    const [form, setForm] = useState({
+        category: '',
+        item: '',
+        amount: '',
+        date: ''
+    })
 
-                    <Form.Group controlId="">
-                        <Form.Label>Item Name</Form.Label>
-                        <Form.Control type="text" placeholder="Item Name" />
-                    </Form.Group>
-                    <Form.Group controlId="">
-                        <Form.Label>Amount</Form.Label>
-                        <Form.Control type="number" placeholder="Amount" />
-                    </Form.Group>
-                    <Form.Group controlId="">
-                        <Form.Label>Date</Form.Label>
-                        <Form.Control type="date" placeholder="date" />
-                    </Form.Group>
-                    <Button variant="primary" type="submit">
-                        Submit
-                </Button>
-                </Form>
-            </div>
-        )
+
+    const { category, item, amount, date } = form;
+
+    const changeHandler = (e) => {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value
+        })
     }
+
+    const formSubmit = async (e) => {
+        e.preventDefault();
+        const newExpense = { category, item, amount, date }
+        try {
+            const config = {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+            const body = newExpense
+            const res = await axios.post('http://localhost:3010/expense/addTransactions', body, config)
+            if (res['data'] === "Success") {
+                window.location.reload()
+
+            }
+        } catch (err) {
+            console.log(err)
+        }
+
+    }
+
+    return (
+        <div>
+            <Form onSubmit={(e) => formSubmit(e)}>
+                <Form.Group controlId="" >
+                    <Form.Label>Category</Form.Label>
+                    <Form.Control type="text" name="category" onChange={e => changeHandler(e)} placeholder="Enter category" />
+                </Form.Group>
+
+                <Form.Group controlId="">
+                    <Form.Label>Item Name</Form.Label>
+                    <Form.Control type="text" name="item" placeholder="Item Name" onChange={e => changeHandler(e)} />
+                </Form.Group>
+                <Form.Group controlId="">
+                    <Form.Label>Amount</Form.Label>
+                    <Form.Control type="number" name="amount" placeholder="Amount" onChange={e => changeHandler(e)} />
+                </Form.Group>
+                <Form.Group controlId="">
+                    <Form.Label>Date</Form.Label>
+                    <Form.Control type="date" name="date" placeholder="date" onChange={e => changeHandler(e)} />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+        </div>
+    )
+
 }
+export default ExpenseForm;
