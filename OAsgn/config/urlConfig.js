7b5362e0a521be5822b42d97f8d8d322f63e1b73
Animesh <animesh.ranchi91@gmail.com>
2019-10-06 19:20:59 +0530

    url changed

--- OAsgn/config/urlConfig.js
+++ OAsgn/config/urlConfig.js
@@ -3,5 +3,5 @@ let env=process.env.NODE_ENV||""
 module.exports=env=="PRO"?{
     dbUrl:"http://localhost:8082"
 }:{
-    dbUrl:"http://localhost:3010"
+    dbUrl:"https://expensereportbyani.herokuapp.com"
 }
